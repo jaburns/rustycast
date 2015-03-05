d83e7f60e6a79967f1cb8b25f276352c3a2fbac6
Jeremy Burns <j@jaburns.net>
2015-03-05 13:44:03 -0700

    Simple effect instead of random pixels

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
 extern crate sdl;
-extern crate rand;
 
 use self::sdl::video::{SurfaceFlag, VideoFlag};
 use self::sdl::event::{Event};
 
+use std::num::Float;
+
 static WIDTH:  usize = 320;
 static HEIGHT: usize = 240;
 
-fn get_color() -> (u8, u8, u8) {
-    (rand::random::<u8>(),
-     rand::random::<u8>(),
-     rand::random::<u8>())
+
+fn get_color(x: usize, y: usize, t: usize) -> (u8, u8, u8) {
+    let fx = x as f32;
+    let fy = y as f32;
+    let dist = Float::sqrt(fx*fx + fy*fy) as u8;
+
+    (dist, 3*(y as u8)-0xFF-10*(t as u8), 7*(t as u8))
 }
+
 
 pub fn real_main() {
     sdl::init(&[sdl::InitFlag::Video]);
     sdl::wm::set_caption("RustyCast", "RustyCast");
 
-    let screen = match sdl::video::set_video_mode(WIDTH as isize, HEIGHT as isize, 32,
+    let screen = match sdl::video::set_video_mode(WIDTH as isize, HEIGHT as isize, 24,
                                                   &[SurfaceFlag::HWSurface],
                                                   &[VideoFlag::DoubleBuf]) {
         Ok(screen) => screen,
@@ -38,11 +43,10 @@ pub fn real_main() {
         screen.with_lock(|pixels| {
             for x in 0..WIDTH {
                 for y in 0..HEIGHT {
-                    let (r, g, b) = get_color();
-                    pixels[4*(WIDTH*y+x) + 0] = 0xFF;
-                    pixels[4*(WIDTH*y+x) + 1] = r;
-                    pixels[4*(WIDTH*y+x) + 2] = g;
-                    pixels[4*(WIDTH*y+x) + 3] = b;
+                    let (r, g, b) = get_color(x, y, t);
+                    pixels[3*(WIDTH*y+x) + 0] = b;
+                    pixels[3*(WIDTH*y+x) + 1] = g;
+                    pixels[3*(WIDTH*y+x) + 2] = r;
                 }
             }
             true
