18100bec3791df7d9bde3c859d1ce681d3fc8d67
Jeremy Burns <j@jaburns.net>
2015-03-18 17:19:36 -0600

    Updated World struct to use wall in different sectors

--- src/world.rs
+++ src/world.rs
@@ -6,13 +6,24 @@ use math::{LineSeg, Vec2, V2_ORIGIN};
 
 
 pub struct World {
-    _walls: Vec<Wall>,
+    _sectors: Vec<Sector>,
+}
+
+pub struct Sector {
+    pub info: SectorInfo,
+    pub walls: Vec<Wall>,
+}
+
+#[derive(Copy)]
+pub struct SectorInfo {
+    pub floor_elev: f32,
+    pub ceiling_elev: f32,
 }
 
 #[derive(Copy)]
 pub struct Wall {
     pub seg: LineSeg,
-    pub height :f32,
+    pub portal: Option<(usize,usize)>, // (sector index, wall index) for matching wall in another sector.
 }
 
 pub struct RayCastResult {
@@ -22,22 +33,61 @@ pub struct RayCastResult {
     pub height: f32,
 }
 
+#[derive(Copy)]
+pub struct NewRayCastResult {
+    pub dist: f32,
+    pub along: f32,
+    pub info: SectorInfo,
+}
+
 
 pub fn temp() -> World {
     World {
-        _walls: vec![
-            Wall::new(-40.0, -40.0,  40.0, -40.0, 10.0),
-            Wall::new( 40.0, -40.0,  40.0,  40.0, 10.0),
-            Wall::new( 40.0,  40.0, -40.0,  40.0, 10.0),
-            Wall::new(-40.0,  40.0, -40.0, -40.0, 10.0),
-            Wall::new(-15.0, -15.0, -10.0, -15.0, 20.0),
-            Wall::new(-10.0, -15.0, -10.0, -10.0, 20.0),
-            Wall::new(-10.0, -10.0, -15.0, -10.0, 20.0),
-            Wall::new(-15.0, -10.0, -15.0, -15.0, 20.0),
-            Wall::new( 15.0,  15.0,  10.0,  15.0, 20.0),
-            Wall::new( 10.0,  15.0,  10.0,  10.0, 20.0),
-            Wall::new( 10.0,  10.0,  15.0,  10.0, 20.0),
-            Wall::new( 15.0,  10.0,  15.0,  15.0, 20.0)
+        _sectors: vec![
+            Sector {
+                info: SectorInfo {
+                    floor_elev: 0.0,
+                    ceiling_elev: 20.0,
+                },
+                walls: vec![
+                    Wall::new(-40.0, -40.0,  40.0, -40.0, None),
+                    Wall::new( 40.0, -40.0,  40.0,  40.0, None),
+                    Wall::new( 40.0,  40.0, -40.0,  40.0, None),
+                    Wall::new(-40.0,  40.0, -40.0, -40.0, None),
+                    Wall::new(-15.0, -15.0, -10.0, -15.0, Some((1,0))),
+                    Wall::new(-10.0, -15.0, -10.0, -10.0, Some((1,1))),
+                    Wall::new(-10.0, -10.0, -15.0, -10.0, Some((1,2))),
+                    Wall::new(-15.0, -10.0, -15.0, -15.0, Some((1,3))),
+                    Wall::new( 15.0,  15.0,  10.0,  15.0, Some((2,0))),
+                    Wall::new( 10.0,  15.0,  10.0,  10.0, Some((2,1))),
+                    Wall::new( 10.0,  10.0,  15.0,  10.0, Some((2,2))),
+                    Wall::new( 15.0,  10.0,  15.0,  15.0, Some((2,3)))
+                ]
+            },
+            Sector {
+                info: SectorInfo {
+                    floor_elev: 5.0,
+                    ceiling_elev: 20.0,
+                },
+                walls: vec![
+                    Wall::new(-15.0, -15.0, -10.0, -15.0, Some((0,4))),
+                    Wall::new(-10.0, -15.0, -10.0, -10.0, Some((0,5))),
+                    Wall::new(-10.0, -10.0, -15.0, -10.0, Some((0,6))),
+                    Wall::new(-15.0, -10.0, -15.0, -15.0, Some((0,7))),
+                ]
+            },
+            Sector {
+                info: SectorInfo {
+                    floor_elev: 5.0,
+                    ceiling_elev: 20.0,
+                },
+                walls: vec![
+                    Wall::new(-15.0, -15.0, -10.0, -15.0, Some((0,8))),
+                    Wall::new(-10.0, -15.0, -10.0, -10.0, Some((0,9))),
+                    Wall::new(-10.0, -10.0, -15.0, -10.0, Some((0,10))),
+                    Wall::new(-15.0, -10.0, -15.0, -15.0, Some((0,11))),
+                ]
+            }
         ]
     }
 }
@@ -48,15 +98,15 @@ pub static W_ZERO: Wall = Wall {
         a: Vec2 { x: 0.0, y: 0.0 },
         b: Vec2 { x: 0.0, y: 0.0 },
     },
-    height: 0.0,
+    portal: None
 };
 
 
 impl Wall {
-    pub fn new(x0: f32, y0: f32, x1: f32, y1: f32, height: f32) -> Wall {
+    pub fn new(x0: f32, y0: f32, x1: f32, y1: f32, portal: Option<(usize,usize)>) -> Wall {
         Wall {
             seg: LineSeg::new(x0, y0, x1, y1),
-            height: height,
+            portal: portal,
         }
     }
 }
@@ -64,7 +114,7 @@ impl Wall {
 
 impl World {
     pub fn get_walls(&self) -> &[Wall] {
-        self._walls.as_slice()
+        self._sectors[0].walls.as_slice()
     }
 
     pub fn cast_ray(&self, pos: Vec2, angle: f32) -> Option<RayCastResult> {
@@ -74,7 +124,7 @@ impl World {
             pos.y - 1000.0*Float::cos(angle)
         );
 
-        let (d2, wall, t) = self._walls.iter()
+        let (d2, wall, t) = self._sectors[0].walls.iter()
             .filter_map(|&wall| ray.intersects_at(wall.seg).map(|t| (wall, t)))
             .fold((f32::MAX, W_ZERO, 0.0), |(s_d2, s_wall, s_t), (wall, t)| {
                 let d2 = (pos - wall.seg.at(t)).get_length_sqr();
@@ -86,10 +136,14 @@ impl World {
                 dist: (pos - wall.seg.at(t)).get_length(),
                 pos: wall.seg.at(t),
                 along: wall.seg.get_length()*t,
-                height: wall.height
+                height: self._sectors[0].info.ceiling_elev
             })
         } else {
             None
         }
+    }
+
+    pub fn new_cast_ray(&self, pos: Vec2, angle: f32) -> Vec<NewRayCastResult> {
+        vec![]
     }
 }
