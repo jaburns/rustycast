51d34e0177a93bfd657950c14a0c2eb263d5d399
Jeremy Burns <j@jaburns.net>
2015-03-18 16:32:21 -0600

    Removed dead code

--- src/game.rs
+++ src/game.rs
@@ -27,8 +27,6 @@ pub struct Game<'a> {
 impl<'a> Game<'a> {
     pub fn step(&mut self, input: &InputState) {
         self.face_angle += input.mouse_dx() / 1000.0;
-        //if input.mouse_dx() < -1.0 { self.face_angle -= TURN; }
-        //if input.mouse_dx() >  1.0 { self.face_angle += TURN; }
 
         if input.has_key(Key::Forward) { self.do_move( 1.0,  0.0); }
         if input.has_key(Key::Back)    { self.do_move(-1.0,  0.0); }
