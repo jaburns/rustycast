8b73987dd24fa04379287c790d4ba5c02f875e43
Jeremy Burns <j@jaburns.net>
2015-03-19 14:00:44 -0600

    Moved wall rendering in to RenderContext, added support for looking up and down

--- src/render.rs
+++ src/render.rs
@@ -14,8 +14,8 @@ const PERSON_HEIGHT: f32 = 5.0;
 
 struct RenderContext<'a> {
     pub pixels: &'a mut [u8],
-    pub width: usize,
-    pub height: usize,
+    pub width: isize,
+    pub height: isize,
 }
 
 
@@ -23,8 +23,8 @@ impl<'a> Game<'a> {
     pub fn render(&self, pixels: &mut [u8], w :usize, h: usize) {
         let mut ctx = RenderContext {
             pixels: pixels,
-            width: w,
-            height: h,
+            width: w as isize,
+            height: h as isize,
         };
 
         if self.show_map {
@@ -41,7 +41,7 @@ impl<'a> Game<'a> {
 
         ctx.clear();
         let (player_x, player_y) = (ctx.width / 2, ctx.height / 2);
-        ctx.put_px(player_x, player_y, 0xFF, 0x00, 0x00);
+        ctx.put_px(player_x as usize, player_y as usize, 0xFF, 0x00, 0x00);
 
         for wall in self.world.get_walls().iter() {
             ctx.draw_seg(wall.seg.transform(trans), 0xFF, 0x00, 0x00);
@@ -49,9 +49,12 @@ impl<'a> Game<'a> {
     }
 
     fn render_game(&self, ctx: &mut RenderContext) {
-        let person_height = PERSON_HEIGHT + Float::abs(Float::sin(self.t)) * 10.0;
-        let w = ctx.width;
-        let h = ctx.height;
+        let person_height = PERSON_HEIGHT; //+ Float::abs(Float::sin(self.t)) * 10.0;
+        let looking_offset = -(self.t * 10.0) as isize;
+        let w = ctx.width as usize;
+        let h = ctx.height as usize;
+
+        ctx.clear();
 
         for x in 0..w {
             let offset = ((x as f32) - (w as f32) / 2.0) / FOV_DIV;
@@ -68,31 +71,11 @@ impl<'a> Game<'a> {
             let cast_dist = dist * Float::cos(offset);
             let pxheight = (VISPLANE_DIST * height / cast_dist) as isize;
 
-            let bottompx = h as isize/2 + (VISPLANE_DIST * person_height / cast_dist) as isize;
-            let toppx_ = bottompx - pxheight;
-            let toppx = if toppx_ >= h as isize { h as isize - 1 } else { toppx_ };
+            let bottom = h as isize / 2 + (VISPLANE_DIST * person_height / cast_dist) as isize + looking_offset;
+            let top = bottom - pxheight;
 
-            for y in 0..toppx {
-                ctx.put_px(x, y as usize, 0x00, 0x00, 0x00);
-            }
-
-            if toppx >= h as isize || bottompx < 0 {
-                continue;
-            }
-
-            let top = if toppx >= 0 { toppx as usize } else { 0 };
-            let bottom = if bottompx < h as isize { bottompx as usize } else { h };
-
-            let brightness = brightness_from_dist(cast_dist);
-
-            for y in top..bottom {
-                let yy = (bottompx as usize - y) as f32 / pxheight as f32 * height / 15.0;
-                let tex_lookup = (along * 25.0) as u8 ^ (512.0*yy) as u8;
-                let color = ((tex_lookup as f32) * brightness) as u8;
-                ctx.put_px(x, y, color / 2, color / 2, color);
-            }
-
-            ctx.draw_floor(x, bottom, h, person_height, self.pos, hit_pos, cast_dist);
+            ctx.draw_wall(x, top, bottom, along, cast_dist);
+            ctx.draw_floor(x, bottom, h as isize, person_height, self.pos, hit_pos, cast_dist, -looking_offset);
         }
     }
 }
@@ -100,14 +83,14 @@ impl<'a> Game<'a> {
 
 impl<'a> RenderContext<'a> {
     pub fn put_px(&mut self, x: usize, y: usize, r: u8, g: u8, b: u8) {
-        self.pixels[3*(self.width*y+x) + 0] = r;
-        self.pixels[3*(self.width*y+x) + 1] = g;
-        self.pixels[3*(self.width*y+x) + 2] = b;
+        self.pixels[3*(self.width as usize*y+x) + 0] = r;
+        self.pixels[3*(self.width as usize*y+x) + 1] = g;
+        self.pixels[3*(self.width as usize*y+x) + 2] = b;
     }
 
     pub fn clear(&mut self) {
-        for x in 0..self.width {
-            for y in 0..self.height {
+        for x in 0..self.width as usize {
+            for y in 0..self.height as usize {
                 self.put_px(x, y, 0x00, 0x00, 0x00);
             }
         }
@@ -121,15 +104,30 @@ impl<'a> RenderContext<'a> {
             let ux = (pt.x + (self.width /2) as f32) as usize;
             let uy = (pt.y + (self.height/2) as f32) as usize;
 
-            if ux < self.width && uy < self.height {
+            if ux < self.width as usize && uy < self.height as usize {
                 self.put_px(ux, uy, r, g, b);
             }
         }
     }
 
-    pub fn draw_floor(&mut self, x: usize, top: usize, bottom: usize, elevation: f32, pos: Vec2, hit_pos: Vec2, cast_dist: f32) {
-        for y in top..bottom {
-            let dist_floor = VISPLANE_DIST * elevation / (y as f32 - self.height as f32 / 2.0);
+    pub fn draw_wall(&mut self, x: usize, top: isize, bottom: isize, along:f32, cast_dist: f32) {
+        let cut_top = if top < 0 { 0 } else { top } as usize;
+        let cut_bottom = if bottom > self.height { self.height } else { bottom } as usize;
+
+        for y in cut_top..cut_bottom {
+            let yy = (bottom as usize - y) as f32 * cast_dist / 5000.0;
+            let tex_lookup = (along * 25.0) as u8 ^ (512.0*yy) as u8;
+            let color = ((tex_lookup as f32) * brightness_from_dist(cast_dist)) as u8;
+            self.put_px(x, y, color / 2, color / 2, color);
+        }
+    }
+
+    pub fn draw_floor(&mut self, x: usize, top: isize, bottom: isize, elevation: f32, pos: Vec2, hit_pos: Vec2, cast_dist: f32, look: isize) {
+        let cut_top = if top < self.height / 2 { self.height / 2 } else { top } as usize;
+        let cut_bottom = if bottom > self.height { self.height } else { bottom } as usize;
+
+        for y in cut_top..cut_bottom {
+            let dist_floor = VISPLANE_DIST * elevation / ((y as isize + look) as f32 - self.height as f32 / 2.0);
             let floor_pos = pos + (hit_pos - pos) * dist_floor / cast_dist;
             let tex_lookup = (floor_pos.x * 10.0) as u8 ^ (floor_pos.y * 10.0) as u8;
             let color = (tex_lookup as f32 * brightness_from_dist(dist_floor)) as u8;
