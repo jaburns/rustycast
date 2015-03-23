f072cba18bfae188279b95c184a4940fedd36d62
Jeremy Burns <j@jaburns.net>
2015-03-23 15:05:08 -0600

    Using Cargo for all dependencies, updated for latest rust nightly

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 #![allow(dead_code)]
 #![feature(core)]
 #![feature(old_io)]
-#![feature(old_path)]
-#![feature(os)]
 #![feature(std_misc)]
 
 extern crate sdl2;
@@ -17,7 +15,7 @@ mod render;
 
 use std::time::Duration;
 use std::old_io::timer;
-use std::os;
+use std::path::Path;
 
 use time::PreciseTime;
 
@@ -38,7 +36,8 @@ const H :usize = 240;
 
 pub fn main() {
     let sdl_context = sdl2::init(sdl2::INIT_VIDEO).unwrap();
-    let res_path = Path::new(format!("{}/../../res", os::self_exe_path().unwrap().as_str().unwrap()));
+
+    let res_path = Path::new("/Users/jaburns/dev/rustycast/res");
 
     let window = match Window::new("RustyCast", WindowPos::PosCentered, WindowPos::PosCentered, WINDOW_WIDTH, WINDOW_HEIGHT, OPENGL) {
         Ok(window) => window,
